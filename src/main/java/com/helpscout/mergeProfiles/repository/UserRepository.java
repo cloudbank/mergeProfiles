9ab01df84379fb4b1622e7b3e90732662ffcb788
droid teahouse <sabine.g.vogel@gmail.com>
2017-11-20 18:44:58 -0800

    Update UserRepository.java

--- src/main/java/com/helpscout/mergeProfiles/repository/UserRepository.java
+++ src/main/java/com/helpscout/mergeProfiles/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.helpscout.mergeProfiles.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.helpscout.mergeProfiles.model.User;
-
+@Repository
 public interface UserRepository extends CrudRepository<User, Long>, UserRepositoryCustom {
 
 }
